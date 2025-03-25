670090780d8951a51fee0e57974ee42d9053f69f
Ronak <ronaksingh2708@gmail.com>
2025-03-25 21:57:29 +0530

    Message added to see command

--- backend/processVideo.js
+++ backend/processVideo.js
@@ -3,11 +3,17 @@ const { exec } = require('child_process');
 function processVideo(inputPath, outputPath) {
   return new Promise((resolve, reject) => {
     const command = `python3 run_model.py "${inputPath}" "${outputPath}"`;
+    console.log(`Executing command: ${command}`);
 
-    console.log("hello world");
     exec(command, (error, stdout, stderr) => {
-      if (error) return reject(error);
-      console.log(`Processing completed: ${stdout}`);
+      if (error) {
+        console.error(`Execution Error: ${error.message}`);
+        console.error(`Standard Error Output: ${stderr}`);
+        return reject(error);
+      }
+      
+      console.log(`Processing completed successfully.`);
+      console.log(`Standard Output: ${stdout}`);
       resolve(outputPath);
     });
   });
